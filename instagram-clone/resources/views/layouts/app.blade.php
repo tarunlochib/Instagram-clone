4ab04822ae079cfe5483e7d95de4072ab72f5e4c
Tarun Lochib <tarunlochib99@gmail.com>
2022-03-25 10:45:01 +0530

    logo setup

--- instagram-clone/resources/views/layouts/app.blade.php
+++ instagram-clone/resources/views/layouts/app.blade.php
@@ -23,8 +23,13 @@
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+                <a class="navbar-brand d-flex" href="{{ url('/') }}">
+                    <div>
+                        <img src="/svg/instagram-seeklogo.com.svg" style="height: 25px; border-right: 1px solid #333333; padding-right: 10px">
+                    </div>
+                    <div style="padding-left: 10px">
+                        Instagram
+                    </div>
                 </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
